8cb2a3e5e452abc3db57bc9ed2aa57ed6e37b1a3
allufsen <allufsensahala@gmail.com>
2022-10-17 11:01:15 +0700

    signin & signout mobile

--- components/Feed.js
+++ components/Feed.js
@@ -1,13 +1,15 @@
-import { SparklesIcon } from "@heroicons/react/24/outline";
+import { ArrowRightOnRectangleIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { useSession, signOut, signIn } from "next-auth/react";
 import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
 
 export default function Feed() {
   const [posts, setPosts] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(
     () =>
@@ -21,9 +23,7 @@ export default function Feed() {
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
-        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
-          <SparklesIcon className="h-5" />
-        </div>
+        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">{session ? <ArrowRightOnRectangleIcon onClick={signOut} className="h-5" /> : <ArrowRightOnRectangleIcon onClick={signIn} className="h-5" />}</div>
       </div>
       <Input />
       <AnimatePresence>
